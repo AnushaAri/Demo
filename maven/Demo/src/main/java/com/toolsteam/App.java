2ebf113c4d7964acb191c3533ee060633d2137a8
AnushaAri <48200114+AnushaAri@users.noreply.github.com>
2019-06-04 09:41:56 +0530

    Update App.java

--- maven/Demo/src/main/java/com/toolsteam/App.java
+++ maven/Demo/src/main/java/com/toolsteam/App.java
@@ -4,10 +4,10 @@ package com.toolsteam;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class A {
+    void foo() {
+    }
+    int bar(int i) {
+        return 0;
     }
 }
